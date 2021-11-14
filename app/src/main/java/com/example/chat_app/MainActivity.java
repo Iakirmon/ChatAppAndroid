6f5e13bcee0f3641b97ccaecf37389620442197b
Iakirmon <wypyszek97@gmail.com>
2021-11-14 20:15:40 +0100

    rejestracja + logowanie + profil + zmiana/resetowania hasla + progressBar

---
+++ app/src/main/java/com/example/chat_app/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.chat_app;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.chat_app.profile.ProfileActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.mnuProfile){
+            startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
