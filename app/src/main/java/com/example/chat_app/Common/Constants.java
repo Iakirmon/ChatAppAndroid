8b541135df0c6ba3feb3f89356c2a3098bbe533f
Iakirmon <wypyszek97@gmail.com>
2022-01-03 18:47:00 +0100

    Dziala wysylanie wiadomosci w UI

--- app/src/main/java/com/example/chat_app/Common/Constants.java
+++ app/src/main/java/com/example/chat_app/Common/Constants.java
@@ -2,8 +2,8 @@ package com.example.chat_app.Common;
 
 public class Constants {
     public static final String IMAGES_FOLDER="images";
-    public static final Object REQUEST_STATUS_SENT = "sent";
-    public static final Object REQUEST_STATUS_RECEIVED = "received";
+    public static final String REQUEST_STATUS_SENT = "sent";
+    public static final String REQUEST_STATUS_RECEIVED = "received";
     public static final String REQUEST_STATUS_ACCEPTED ="accepted" ;
     public static final String MESSAGE_TYPE_TEXT = "text";
 }
