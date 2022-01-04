8a9d1af8029bf6ed5aca39d91682765321998b6d
Iakirmon <wypyszek97@gmail.com>
2022-01-04 11:47:04 +0100

    Wysylanie plikow - zdjec, filmow w konwersacji i do firebase

--- app/src/main/java/com/example/chat_app/chats/ChatActivity.java
+++ app/src/main/java/com/example/chat_app/chats/ChatActivity.java
@@ -3,16 +3,27 @@ package com.example.chat_app.chats;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.chat_app.Common.Constants;
@@ -20,6 +31,11 @@ import com.example.chat_app.Common.Extras;
 import com.example.chat_app.Common.NodeNames;
 import com.example.chat_app.Common.Util;
 import com.example.chat_app.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -28,14 +44,19 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ServerValue;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class ChatActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageView ivSend;
+    private ImageView ivSend,ivAttachment;
     private EditText etMessage;
     private DatabaseReference mRootRef;
     private FirebaseAuth firebaseAuth;
@@ -48,17 +69,29 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
 
     private int currentPage=1;
     private static final int RECORD_PER_PAGE=30;
+
+    private static final int REQUEST_CODE_PICK_IMAGE=101;
+    private static final int REQUEST_CODE_PICK_VIDEO=103;
+    private static final int REQUEST_CODE_CAPTURE_IMAGE=102;
     private DatabaseReference databaseReferenceMessages;
     private ChildEventListener childEventListener;
 
+    private BottomSheetDialog bottomSheetDialog;
+
+    private LinearLayout llProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         ivSend = findViewById(R.id.ivSend);
+        ivAttachment=findViewById(R.id.ivAttachment);
         etMessage=findViewById(R.id.etMessage);
+
+        llProgress=findViewById(R.id.llProgress);
+
         ivSend.setOnClickListener(this);
+        ivAttachment.setOnClickListener(this);
         firebaseAuth = FirebaseAuth.getInstance();
         mRootRef = FirebaseDatabase.getInstance().getReference();
         currentUserId=firebaseAuth.getCurrentUser().getUid();
@@ -82,6 +115,13 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+        bottomSheetDialog = new BottomSheetDialog(this);
+        View view = getLayoutInflater().inflate(R.layout.chat_file_options,null);
+        view.findViewById(R.id.llCamera).setOnClickListener(this);
+        view.findViewById(R.id.llGallery).setOnClickListener(this);
+        view.findViewById(R.id.llVideo).setOnClickListener(this);
+        view.findViewById(R.id.ivClose).setOnClickListener(this);
+        bottomSheetDialog.setContentView(view);
     }
     private void sendMessage(String msg, String msgType, String pushId){
         try{
@@ -172,6 +212,170 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
                     Toast.makeText(this,R.string.no_internet,Toast.LENGTH_SHORT).show();
                 }
                 break;
+            case R.id.ivAttachment:
+                if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED){
+                    if(bottomSheetDialog!=null){
+                        bottomSheetDialog.show();
+
+                    }
+                } else {
+                    ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+                }
+                InputMethodManager inputMethodManager=(InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                if(inputMethodManager!=null){
+                    inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(),0);
+                }
+                break;
+            case R.id.llCamera:
+                bottomSheetDialog.dismiss();
+                Intent intentCamera= new Intent(Intent.ACTION_INPUT_METHOD_CHANGED);
+                startActivityForResult(intentCamera,REQUEST_CODE_CAPTURE_IMAGE);
+                break;
+            case R.id.llGallery:
+                bottomSheetDialog.dismiss();
+                Intent intentImage=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intentImage,REQUEST_CODE_PICK_IMAGE);
+                break;
+            case R.id.llVideo:
+                bottomSheetDialog.dismiss();
+                Intent intentVideo=new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intentVideo,REQUEST_CODE_PICK_VIDEO);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK){
+            if(requestCode==REQUEST_CODE_CAPTURE_IMAGE){
+                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                ByteArrayOutputStream bytes=new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG,100,bytes);
+                uploadBytes(bytes,Constants.MESSAGE_TYPE_IMAGE);
+
+            } else if(requestCode==REQUEST_CODE_PICK_IMAGE){
+                Uri uri = data.getData();
+                uploadFile(uri,Constants.MESSAGE_TYPE_IMAGE);
+            } else if(requestCode==REQUEST_CODE_PICK_VIDEO){
+                Uri uri = data.getData();
+                uploadFile(uri,Constants.MESSAGE_TYPE_VIDEO);
+            }
+        }
+    }
+
+    private void uploadFile(Uri uri, String messageType) {
+        DatabaseReference databaseReference = mRootRef.child(NodeNames.MESSAGES).child(currentUserId).child(chatUserId).push();
+        String pushId = databaseReference.getKey();
+
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+        String folderName = messageType.equals(Constants.MESSAGE_TYPE_VIDEO) ? Constants.MESSAGE_VIDEOS : Constants.MESSAGE_IMAGES;
+        String fileName = messageType.equals(Constants.MESSAGE_TYPE_VIDEO) ? pushId + ".mp4" : pushId + ".jpg";
+
+        StorageReference fileRef = storageReference.child(folderName).child(fileName);
+        UploadTask uploadTask = fileRef.putFile(uri);
+
+        uploadProgress(uploadTask, fileRef, pushId, messageType);
+    }
+
+    private void uploadBytes(ByteArrayOutputStream bytes, String messageType) {
+        DatabaseReference databaseReference = mRootRef.child(NodeNames.MESSAGES).child(currentUserId).child(chatUserId).push();
+        String pushId = databaseReference.getKey();
+
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+        String folderName = messageType.equals(Constants.MESSAGE_TYPE_VIDEO) ? Constants.MESSAGE_VIDEOS : Constants.MESSAGE_IMAGES;
+        String fileName = messageType.equals(Constants.MESSAGE_TYPE_VIDEO) ? pushId + ".mp4" : pushId + ".jpg";
+
+        StorageReference fileRef = storageReference.child(folderName).child(fileName);
+        UploadTask uploadTask = fileRef.putBytes(bytes.toByteArray());
+        uploadProgress(uploadTask, fileRef, pushId, messageType);
+    }
+
+    private void uploadProgress(final UploadTask task, final StorageReference filePath, final String pushId, final String messageType) {
+
+        final View view = getLayoutInflater().inflate(R.layout.file_progress, null);
+        final ProgressBar pbProgress = view.findViewById(R.id.pbProgress);
+        final TextView tvProgress = view.findViewById(R.id.tvFileProgress);
+        final ImageView ivPlay = view.findViewById(R.id.ivPlay);
+        final ImageView ivPause = view.findViewById(R.id.ivPause);
+        ImageView ivCancel = view.findViewById(R.id.ivCancel);
+
+        ivPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                task.pause();
+                ivPlay.setVisibility(View.VISIBLE);
+                ivPause.setVisibility(View.GONE);
+            }
+        });
+
+        ivPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                task.resume();
+                ivPause.setVisibility(View.VISIBLE);
+                ivPlay.setVisibility(View.GONE);
+            }
+        });
+
+        ivCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                task.cancel();
+            }
+        });
+
+        llProgress.addView(view);
+        tvProgress.setText(R.string.upload_progress);
+
+        task.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+
+                pbProgress.setProgress((int) progress);
+                tvProgress.setText(R.string.upload_progress);
+
+            }
+        });
+
+        task.addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                llProgress.removeView(view);
+                if (task.isSuccessful()) {
+                    filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            String downloadUrl = uri.toString();
+                            sendMessage(downloadUrl, messageType, pushId);
+                        }
+                    });
+                }
+            }
+        });
+
+        task.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                llProgress.removeView(view);
+                Toast.makeText(ChatActivity.this, R.string.failed_to_upload, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (bottomSheetDialog != null)
+                    bottomSheetDialog.show();
+            } else {
+                Toast.makeText(this, "Wymagane uprawnienia dostępu do plików", Toast.LENGTH_SHORT).show();
+            }
+
         }
     }
 }
